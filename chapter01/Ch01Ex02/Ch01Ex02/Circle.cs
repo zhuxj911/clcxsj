508e994e1996db7fb6789f17d506b9326effb41a
zhu xj <xazhuxj@qq.com>
2018-03-11 23:09:32 +0800

    增加充实了第1章的内容和示例

--- chapter01/Ch01Ex02/Ch01Ex02/Circle.cs
+++ chapter01/Ch01Ex02/Ch01Ex02/Circle.cs
@@ -4,96 +4,31 @@ namespace Ch01Ex02
 {
     class Point
     {
-        private string name;
-        public string Name
-        {
-            get { return name; }
-            set { name = value; }
-        }
+        public string name;
+        public double x;
+        public double y;
+        public double z;
 
-        private double x;
-        public double X
-        {
-            get { return x; }
-            set { x = value; }
-        }
-        private double y;
-        public double Y
-        {
-            get { return y; }
-            set { y = value; }
-        }
-        private double z;
-        public double Z
-        {
-            get { return z; }
-            set { z = value; }
-        }
-
-        public Point()
-        {
-            Name = "";
-            x = y = z = 0;
-        }
-        //函数重载：函数名称一样，但参数（个数或类型）不一样
-        public Point(string name, double x, double y, double z)
-        {
-            this.name = name;
-            this.x = x;
-            this.y = y;
-            this.z = z;
-        }
-
-
-        /// <summary>
-        /// 计算当前点至p2点的距离
-        /// </summary>
-        /// <param name="p2">目标点</param>
-        /// <returns>两点的距离</returns>
         public double Distance(Point p2)
         {
-            double dx = X - p2.X;
-            double dy = Y - p2.Y;
+            double dx = x - p2.x;
+            double dy = y - p2.y;
             return Math.Sqrt(dx * dx + dy * dy);
         }
     }
 
     class Circle
     {
-        private Point center;
-        private double r;
+        public Point center;
+        public double r;
+        public double area;
+        public double length;
 
-        private double area;
-        public double Area
-        {
-            get { return area; }
-        }
-
-        private double length;
-
-        public Circle()
-        {
-           
-        }
-
-        public Circle(string centerName, double x, double y, double z, double r)
-        {
-            center = new Point(centerName, x, y, z);
-            this.r = r;
-
-            CalArea();
-        }
-
-
-        /// <summary>
-        /// 计算圆的面积
-        /// </summary>
-        private void CalArea()
+        public void Area()
         {
             area = Math.PI * r * r;
         }
 
-        //判断两圆是否相交
         public bool IsIntersectWithCircle(Circle c2)
         {
             double d = this.center.Distance(c2.center);
--- chapter01/Ch01Ex02/Ch01Ex02/Program.cs
+++ chapter01/Ch01Ex02/Ch01Ex02/Program.cs
@@ -6,11 +6,32 @@ namespace Ch01Ex02
     {
         static void Main(string[] args)
         {
-            Circle c1 = new Circle("pt1", 100, 100, 425.324, 80);
-            Circle c2 = new Circle("pt2", 200, 200, 417.626, 110);
+            Point p1 = new Point();
+            p1.name = "p1";
+            p1.x = 100;
+            p1.y = 100;
+            p1.z = 425.324;
 
-            Console.WriteLine("Circle1的面积={0}", c1.Area );
-            Console.WriteLine("Circle2的面积={0}", c2.Area);
+
+            Circle c1 = new Circle();
+            c1.center = p1;
+            c1.r = 80;
+
+            Point p2 = new Point();
+            p2.name = "p2";
+            p2.x = 200;
+            p2.y = 200;
+            p2.z = 417.626;
+
+            Circle c2 = new Circle();
+            c2.center = p2;
+            c2.r = 110;
+
+            c1.Area(); //计算圆c1的面积
+            Console.WriteLine("Circle1的面积={0}", c1.area );
+
+            c2.Area(); //计算圆c2的面积
+            Console.WriteLine("Circle2的面积={0}", c2.area);
 
             bool yes = c1.IsIntersectWithCircle(c2);
             Console.WriteLine("Circle1与Circle2是否相交:{0}", 

a8f90a66971e8686e6d384a88e54fb0a7c7535bd
zhu xj <xazhuxj@qq.com>
2018-03-11 23:08:36 +0800

    修改了第1章内容

---
+++ chapter01/Ch01Ex03/Ch01Ex03/Circle.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace Ch01Ex03
+{
+    class Point
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private double x;
+        public double X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        private double y;
+        public double Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        private double z;
+        public double Z
+        {
+            get { return z; }
+            set { z = value; }
+        }
+
+        public Point(string name, double x, double y, double z)
+        {
+            this.name = name;
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public double Distance(Point p2)
+        {
+            double dx = X - p2.X;
+            double dy = Y - p2.Y;
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+    }
+
+    class Circle
+    {
+        public Point Center { get; set; }
+
+        private double r;
+        public double R
+        {
+            get { return r; }
+            set
+            {
+                if(value != r)
+                {
+                    r = value;
+                    CalArea(); //r的值发生改变，重新计算圆的面积
+                }
+            }
+        }
+
+        private double area;
+        public double Area
+        {
+            get { return area; }
+        }
+
+        private double length;
+
+        public Circle(string centerName, double x, double y, double z, double r)
+        {
+            Center = new Point(centerName, x, y, z);
+
+            this.r = r;
+            CalArea(); //r的值确定，计算圆的面积
+        }
+
+        //计算圆的面积
+        private void CalArea()
+        {
+            area = Math.PI * r * r;
+        }
+
+        //判断两圆是否相交
+        public bool IsIntersectWithCircle(Circle c2)
+        {
+            double d = this.Center.Distance(c2.Center);
+            return d <= (r + c2.r);
+        }
+    }
+}
---
+++ chapter01/Ch01Ex04/Ch01Ex04/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ch01Ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c1 = new Circle("pt1", 100, 100, 425.324, 80);
+            Circle c2 = new Circle("pt2", 200, 200, 417.626, 110);
+
+            Console.WriteLine("Circle1的面积={0}", c1.Area );
+            Console.WriteLine("Circle2的面积={0}", c2.Area);
+
+            bool yes = c1.IsIntersectWithCircle(c2);
+            Console.WriteLine("Circle1与Circle2是否相交:{0}", 
+				yes ? "是" : "否" );
+        }
+    }
+}

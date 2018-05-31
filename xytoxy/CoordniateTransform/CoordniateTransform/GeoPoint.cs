b0d8ea7be932066472878e401f2430e0258fffd3
xazhuxj <xazhuxj@qq.com>
2018-06-01 06:24:45 +0800

    修改了平面转标转换一章，还有待进一步的修改

---
+++ xytoxy/CoordniateTransform/CoordniateTransform/GeoPoint.cs
@@ -0,0 +1,67 @@
+﻿namespace CoordniateTransform
+{
+    public class GeoPoint : NotificationObject
+    {
+        private string _name;
+        public string Name //点名
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                RaisePropertyChange("Name");
+            }
+        }       
+
+        private double _oX;
+        public double OX //源X坐标
+        {
+            get { return _oX; }
+            set
+            {
+                _oX = value;
+                RaisePropertyChange("OX");
+            }
+        }
+  
+        private double _oY;
+        public double OY  //源Y坐标
+        {
+            get { return _oY; }
+            set
+            {
+                _oY = value;
+                RaisePropertyChange("OY");
+            }
+        }
+
+
+        private double _nX;
+        public double NX //新X坐标
+        {
+            get { return _nX; }
+            set
+            {
+                _nX = value;
+                RaisePropertyChange("NX");
+            }
+        }
+
+        private double _nY;
+        public double NY  //新Y坐标
+        {
+            get { return _nY; }
+            set
+            {
+                _nY = value;
+                RaisePropertyChange("NY");
+            }
+        }
+
+ 
+        public override string ToString()
+        {
+            return string.Format("{0},{1:0.000},{2:0.000},{3:0.000},{4:0.000}", Name, OX, OY, NX, NY);
+        }
+    }
+}
---
+++ xytoxy/CoordniateTransform/CoordniateTransform/SMath.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace ZXY
+{
+    public static class SMath
+    {
+        /// <summary>
+        /// 将60进制的度分秒角度转换为弧度
+        /// </summary>
+        /// <param name="dmsAngle">形如ddd.mmss的60进制的角度</param>
+        /// <returns>弧度值</returns>
+        public static double DMS2RAD(double dmsAngle)
+        {
+            dmsAngle *= 1e4;
+            int dms = (int)Math.Round(dmsAngle);
+            int d = dms / 10000;
+            int m = (dms - d * 10000) / 100;
+            double s = dmsAngle - d * 1e4 - m * 1e2;
+            return  (d + m / 60.0 + s / 3600.0)/180.0*Math.PI;
+        }
+
+        ///// <summary>
+        ///// 将弧度转换为60进制的度分秒角度
+        ///// </summary>
+        ///// <param name="radAngle">单位为弧度的角度</param>
+        ///// <returns>60进制的角度</returns>
+        //public static double RAD2DMS(double radAngle)
+        //{
+        //    radAngle = radAngle / Math.PI * 180 * 3600;
+        //    int sAngle = (int)Math.Round(radAngle);
+
+        //    int d = sAngle / 3600;
+        //    int m = (sAngle - d * 3600) / 60;
+        //    double s = radAngle - d * 3600 - m * 60.0;
+
+        //    return d + m / 100.0 + s / 10000.0;
+        //}
+
+        public static void RAD2DMS(double radAngle,
+            out int d, out int m, out double s)
+        {
+            radAngle = radAngle / Math.PI * 180 * 3600;
+            int sAngle = (int)Math.Round(radAngle);
+
+            d = sAngle / 3600;
+            m = (sAngle - d * 3600) / 60;
+            s = radAngle - d * 3600 - m * 60.0;
+        }
+
+        /// <summary>
+        /// 将弧度转换为60进制的度分秒角度
+        /// </summary>
+        /// <param name="radAngle">单位为弧度的角度</param>
+        /// <returns>60进制的角度</returns>
+        public static double RAD2DMS(double radAngle)
+        {
+            double s;
+            int d, m;
+
+            RAD2DMS(radAngle, out d, out m, out s);
+            return d + m / 100.0 + s / 10000.0;
+        }
+
+        public static string RAD2DMSString(double radAngle)
+        {
+            double s;
+            int d, m;
+
+            RAD2DMS(radAngle, out d, out m, out s);
+            return string.Format("{0}°{1}′{2}″", d, m, s); ;
+        }
+    }
+}
